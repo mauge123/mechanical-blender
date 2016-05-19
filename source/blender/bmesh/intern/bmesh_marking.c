8d557cc9f1beb83ac799e5d4c415871e35bfc9c6
Jaume Bellet <mauge@bixo.org>
2016-05-19 07:34:27 +0200

    Mechanical: Not select vertex of dimension if the mode is set to dimension

--- source/blender/bmesh/intern/bmesh_marking.c
+++ source/blender/bmesh/intern/bmesh_marking.c
@@ -506,11 +506,13 @@ void BM_dim_select_set(BMesh *bm, BMDim *d, const bool select)
 			BM_elem_flag_enable(d, BM_ELEM_SELECT);
 			bm->totdimsel += 1;
 
-			// Select all vertices
-			for (int i=0; i< d->totverts; i++) {
-				if (!BM_elem_flag_test(d->v[i], BM_ELEM_SELECT)) {
-					bm->totvertsel += 1;
-					BM_elem_flag_enable(d->v[i], BM_ELEM_SELECT);
+			if (bm->selectmode != SCE_SELECT_DIMENSION) {
+				// Select all vertices
+				for (int i=0; i< d->totverts; i++) {
+					if (!BM_elem_flag_test(d->v[i], BM_ELEM_SELECT)) {
+						bm->totvertsel += 1;
+						BM_elem_flag_enable(d->v[i], BM_ELEM_SELECT);
+					}
 				}
 			}
 		}
